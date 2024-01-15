75985bf656827b8a0014f29e3c1a45826391f43d
Gaurav.S.Srivastava <gauravsrivastava1501@gmail.com>
2024-01-15 22:43:07 +0530

    user signin repo

--- api/user/user.repository.js
+++ api/user/user.repository.js
@@ -1,15 +1,22 @@
 import { userSchema } from "./user.schema";
 import mongoose from "mongoose";
 
-const UserModel = mongoose.model('User', userSchema)
+const UserModel = mongoose.model("User", userSchema);
 
-export const userSignUpRepo = async (userData) =>{
-    try{
-        
-        const newUser = new UserModel(userData)
-        await newUser.save()
-        return {success:true, res: newUser}
-    }catch(err){
-        return {success: false, error: {statusCode: 500, msg: err}}
-    }
-}
+export const userSignUpRepo = async (userData) => {
+  try {
+    const newUser = new UserModel(userData);
+    await newUser.save();
+    return { success: true, res: newUser };
+  } catch (err) {
+    return { success: false, error: { statusCode: 500, msg: err } };
+  }
+};
+
+export const userSingInRepo = async (userData) => {
+  try {
+    const existingUser = await UserModel.find({ email: userData.email });
+  } catch (err) {
+    return { success: false, error: { statusCode: 500, msg: err } };
+  }
+};
